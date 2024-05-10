16930d7b615dbee97f9106d60682f7d07d326e01
Philipp Hergenhahn <philipp.hergenhahn@setilic.com>
2024-05-10 21:26:13 +0200

    Initial Commit

---
+++ app/src/main.tsx
@@ -0,0 +1,10 @@
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {Router} from "./router.tsx";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <Router/>
+    </React.StrictMode>,
+)
