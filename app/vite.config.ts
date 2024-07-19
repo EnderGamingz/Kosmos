92ae8270d9ed9c9b2657c6d4f10a213e554e5de5
Philipp Hergenhahn <philipphergenhahn@gmail.com>
2024-07-19 13:01:08 +0200

    Add PWA support with new favicon and images
    The commit includes the addition of PWA support to the application. It introduces a new favicon and images for use within the app. The manifest.json file is also added, which entails PWA configuration and specifies details about the application. Resources like the SVG logo file have been added and are referenced in the manifest file. The vite.config.ts file is amended to include PWA-related functionality.

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,8 +2,32 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import webfontDownload from 'vite-plugin-webfont-dl';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
+
+import Manifest from './public/manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), webfontDownload(), tsconfigPaths()],
+  plugins: [
+    react(),
+    webfontDownload(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'img/logo_filled.svg',
+        'img/logo_filled_full.svg',
+        'img/logo_outline_white.svg',
+        'img/logo_outline_full.svg',
+        'img/logo_outline.svg',
+        'img/apple-touch-icon.png',
+        'img/logo_outline_full_white_bg.svg',
+        'img/logo_outline_full_white_bg_512.png',
+        'img/logo_outline_full_white_bg_192.png',
+        'img/favicon.ico',
+        'img/favicon.svg',
+      ],
+      manifest: Manifest as never,
+    }),
+  ],
 });
