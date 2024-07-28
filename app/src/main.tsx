382860356a6a2fa06da44cf9cbc8a9bb244614f6
Philipp Hergenhahn <philipp.hergenhahn@setilic.com>
2024-07-28 13:42:03 +0200

    Refactor stream-saver integration and file structure
    Updated the file structure for the stream management utility under the `public` directory. This includes renaming and relocating several related files for better organization.

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,13 +4,9 @@ import axios from 'axios';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@lib/query.ts';
 import { NextUIProvider } from '@nextui-org/react';
-import streamSaver from 'streamsaver';
 import LazyRouter from './lazyRouter.tsx';
 
 axios.defaults.withCredentials = true;
-
-// Self-host stream-savers man in the middle scripts for writing streams to file
-streamSaver.mitm = '/stream/mitm.html';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
