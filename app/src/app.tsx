13fe12eef988f30dc8ac477a5e33c24c9fcb904c
Philipp Hergenhahn <philipphergenhahn@gmail.com>
2024-09-08 14:46:15 +0200

    Removing dark mode test button
    Removed the dark mode toggle button from the app.

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -4,11 +4,9 @@ import { queryClient } from '@lib/query.ts';
 import { NextUIProvider } from '@nextui-org/react';
 import LazyRouter from './lazyRouter.tsx';
 import { useEffect } from 'react';
-import { SunIcon } from '@heroicons/react/24/outline';
 
 export function App() {
   const darkMode = usePreferenceStore(s => s.theme.type === Theme.Dark);
-  const set = usePreferenceStore(s => s.theme.setType);
 
   useEffect(() => {
     document.body.classList.toggle('dark', darkMode);
@@ -21,11 +19,6 @@ export function App() {
           'app-wrapper flex flex-grow flex-col ' + (darkMode ? 'dark' : '')
         }>
         <LazyRouter />
-        <button
-          className={'fixed bottom-3 right-3 z-[600]'}
-          onClick={() => set(darkMode ? Theme.Light : Theme.Dark)}>
-          <SunIcon className={'h-5 w-5'} />
-        </button>
       </NextUIProvider>
     </QueryClientProvider>
   );
