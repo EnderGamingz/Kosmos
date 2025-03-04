156fba130a0f6bb3ee8e2434ff844371a67c8b55
Philipp Hergenhahn <philipphergenhahn@gmail.com>
2025-03-04 12:09:57 +0100

    Added missing ui components

---
+++ app/src/components/ui/skeleton.tsx
@@ -0,0 +1,14 @@
+import { cn } from '@/lib/utils';
+import { ComponentProps } from 'react';
+
+function Skeleton({ className, ...props }: ComponentProps<'div'>) {
+  return (
+    <div
+      data-slot='skeleton'
+      className={cn('bg-primary/10 animate-pulse rounded-md', className)}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
