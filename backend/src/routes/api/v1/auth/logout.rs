3db0e2b5ce1e8f8ad5f19aded8a8d81c4289e1c7
Philipp Hergenhahn <philipphergenhahn@gmail.com>
2025-05-02 19:59:19 +0200

    Refactor logout to use JWT claims instead of sessions
    Replaced session-based authentication with JWT claims in the logout handler. Removed dependencies on session management and updated the logic accordingly to streamline authentication flow. This simplifies the code and improves consistency with JWT-based authentication.

--- backend/src/routes/api/v1/auth/logout.rs
+++ backend/src/routes/api/v1/auth/logout.rs
@@ -1,16 +1,7 @@
-use tower_sessions::Session;
-
-use crate::response::error_handling::AppError;
+use crate::model::jwt::JwtClaims;
 use crate::response::success_handling::{AppSuccess, ResponseResult};
-use crate::services::session_service::SessionService;
+use axum_jwt_auth::Claims;
 
-pub async fn logout(session: Session) -> ResponseResult {
-    let session_exists = SessionService::get_user_id(&session).await;
-
-    if session_exists.is_none() {
-        return Err(AppError::NotLoggedIn);
-    }
-
-    SessionService::flush_session(&session).await;
+pub async fn logout(Claims(_claims): Claims<JwtClaims>) -> ResponseResult {
     Ok(AppSuccess::OK { data: None })
 }
