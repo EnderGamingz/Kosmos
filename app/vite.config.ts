7804fd204db3c9bcf2462d72c79a5fddf51091ab
Philipp Hergenhahn <philipphergenhahn@gmail.com>
2026-04-14 11:17:28 +0200

    Update dependencies for axios and vite, remove unused vite-tsconfig-paths plugin

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,25 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import webfontDownload from 'vite-plugin-webfont-dl';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'node:path';
 
 export default defineConfig({
   envPrefix: ['VITE_'],
-  plugins: [react(), tailwindcss(), webfontDownload(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), webfontDownload()],
   resolve: {
+    tsconfigPaths: true,
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 1000,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
     },
   },
 });
